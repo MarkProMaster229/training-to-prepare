a7e8828fa01945c5d7fe6ec604111f0f531d1135
markpromaster229 <romtomgmai@gmail.com>
2026-05-03 14:02:14 +0300

    fullLabel

--- MOCK/MockForRender.py
+++ MOCK/MockForRender.py
@@ -8,6 +8,34 @@ def forlabel():
     return jsonify({"message": "Успешно"}), 200
 
 
+@app.route('/server', methods=['POST'])
+def chekMe():
+    print("im Work!")
+    data = request.get_json()
+    print(data)
+    return jsonify({"message": "Успешно"}), 200
 
+@app.route('/serverGET', methods=['GET'])
+def uploadData():
+    print("chekme")
+    return jsonify({
+        'mess': [
+            {
+                'user_name': "testName",
+                'message': "ты же знаешь!",
+                'data_ser': "01.12.1999"
+            },
+            {
+                'user_name': "я уже тут",
+                'message': "ты ждал меня!",
+                'data_ser': "02.11.1997"
+            },
+            {
+                'user_name': "обернись",
+                'message': "смотри! это я!",
+                'data_ser': "07.01.1996"
+            }
+        ]
+    })
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/frontend_work/src/Communication.jsx
+++ frontend/frontend_work/src/Communication.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,14 +7,54 @@ import './Communication.css'
 //↓↓↓↓↓↓обрати внимание сюда↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 function Communication({settopic, nameUser}){// смотри как я передаю имя
 //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    const [user_server, set_user_server] = useState([])
+    const [message_server, set_message] = useState([])
+    const [data_server, set_data] = useState([])
+    const [messages, setMessages] = useState([])
+    useEffect (() => {
+        fetch('http://localhost:5000/serverGET', {
+            method : 'GET'
+        })
+            .then(response => response.json())
+            .then(data => {
+                setMessages(data.mess)
+            })
+        }, [])
 
+
+    const [message, setMessage] = useState('')
+    const upload_to_the_server = () =>{
+        fetch('http://localhost:5000/server',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "userName" : nameUser,
+                //предполагаю что дата формируется внутри самой бд у тебя как я видел есть поле date_create внутри ChatTalk для этого
+                "topic" : "communication",
+                "message" : message,
+            })
+        })
+    }
+//----------------------------------
+function renderMessage(){
+    return messages.map((msg, i) => (
+    <p key={i}>{msg.data_ser} | {msg.user_name}: {msg.message}</p>
+))
+//----------------------------------
+}
 
 
     return(
         <>
 <div>
     <div className='laberFor' > 
-        <h1>{nameUser}</h1>
+        <h2>введите свое сообщение для публикации на форуме</h2>
+        <h2>ваш nickname привлекателен {nameUser}</h2>
+        {renderMessage()}
+        <input type='text' value={message} onChange={(e) => setMessage(e.target.value)}/>
+        <button onClick={upload_to_the_server}>я готов нести ответственность за свои поступки</button>
     </div>
 
 
