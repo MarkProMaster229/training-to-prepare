2b58fbd57e851ea0df1438064717524706527769
markpromaster229 <romtomgmai@gmail.com>
2026-05-06 10:51:49 +0300

    1

--- backend_rust/src/main.rs
+++ backend_rust/src/main.rs
@@ -9,13 +9,15 @@ async fn hello() -> &'static str {
 async fn run_server() {
     let cors = CorsLayer::new()
         .allow_origin(Any)
-        .allow_methods([[Method::GET, Method::POST]])
+        .allow_methods([Method::GET, Method::POST])
         .allow_headers(Any);
 
     let app = Router::new()
         .route("/regist", any(hello))
         .layer(cors);
 
+    //let app = Router::new()
+    //    .route("/")
     let listener = tokio::net::TcpListener::bind("127.0.0.1:5000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
